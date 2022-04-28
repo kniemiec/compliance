6e96e31307b1657db186cf0352eb434630bd7154
Krzysztof Niemiec <krzysztof.niemiec@pi-software.com>
2022-04-28 17:00:49 +0200

    REST service implementation started

--- compliance-server.go
+++ compliance-server.go
@@ -1,58 +1,35 @@
 package main
 
-import (
-	"context"
-	"flag"
-	"fmt"
-	"log"
-	"net"
+import (	
+	"net/http"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-
-	pb "compliance-service/compliance"
+    "github.com/gin-gonic/gin"
 )
 
-
-var (
-	port = flag.Int("port", 9999, "The server port")
-)
-
-type complianceCheckServerStruct struct {
-	pb.UnimplementedComplianceCheckServer
+type ComplianceCheckRequest struct {
+	transferId string `json:"transferId"`
+	// senderData UserData 'json:"senderData"'
+	// receiverData UserData 'json:"receiverData"'
 }
 
-func (s *complianceCheckServerStruct) CheckCompliance(ctx context.Context, in *pb.ComplianceCheckRequest) (*pb.ComplianceCheckResponse, error) {
-	log.Println("request received: " + in.TransferId)
-	response := &pb.ComplianceCheckResponse{}
-	response.TransferId = in.TransferId
-	response.ComplianceStatus = "OK"
-	return response, nil
+type ComplianceCheckResponse struct {
+	transferId string `json:"transferId"`
+	// senderData UserData 'json:"senderData"'
+	// receiverData UserData 'json:"receiverData"'
 }
 
-func (s *complianceCheckServerStruct) Hello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
-	log.Println("request received: " + in.Message)
-	response := &pb.HelloResponse{}
-	response.Message = "It"
-	return response, nil
+var status = ComplianceCheckRequest {
+	transferId: "1",}
+
+func getComplianceStatus(c * gin.Context){
+	c.IndentedJSON(http.StatusOK, status)
 }
 
-func newServer() *complianceCheckServerStruct {
-	log.Println("Server created.")
-	return &complianceCheckServerStruct{}
-}
 
 func main() {
-	log.Println("Compliance service is starting....")
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	log.Println("Listening on port: ", *port)
-	grpcServer := grpc.NewServer()
-	pb.RegisterComplianceCheckServer(grpcServer, newServer())
-	reflection.Register(grpcServer)
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	router := gin.Default()
+	router.GET("complianceStatus", getComplianceStatus)
+
+	router.Run("localhost:8888")
+
 }
