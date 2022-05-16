22c3913a7e42fe62914041d58d559c19e392906a
Krzyszto Niemiec <krzysztof.niemiec@gmail.com>
2022-05-16 15:56:53 +0200

    Kubernetes manifest added. Response form service fixed.

--- compliance-service.go
+++ compliance-service.go
@@ -21,13 +21,22 @@ var sanctionedUsers = []SanctionedUser{
 
 func (service ComplianceServiceImpl) check(request ComplianceCheckRequest) ComplianceCheckResponse {
 	response := ComplianceCheckResponse{ }
+	problemsDetected := false
+	response.TransferId = request.TransferId
 	for _, user := range sanctionedUsers {
 		if user.lastname == request.SenderData.Lastname && user.surname == request.SenderData.Surname {
 			response.SenderStatus = append(response.SenderStatus, ComplianceProblem { ProblemId: "1", ProblemDescription : "User on sanctions list"})
+			problemsDetected = true
 		}
 		if user.lastname == request.ReceiverData.Lastname && user.surname == request.ReceiverData.Surname {
 			response.ReceiverStatus = append(response.ReceiverStatus, ComplianceProblem { ProblemId: "1", ProblemDescription : "User on sanctions list"})
+			problemsDetected = true
 		}
+	}
+	if problemsDetected {
+		response.Status = "ALERT"
+	} else {
+		response.Status = "OK"
 	}
 	return response;
 }
